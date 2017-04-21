9cbc63e75d092ee9c387470517749314669cba29
郑康 <1473345713@qq.com>
2017-04-21 20:45:23 +0800

    是不是太胖了

---
+++ 029.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int main()
+{
+    int n;
+    float m;
+    cin>>n;
+    m=(n-100.0)*0.9*2;
+    printf("%.1f",m);
+    return 0;
+}
+
